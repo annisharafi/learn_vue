7e03ffe6ed24bb8947292eda553866e28234365c
annisha <channisha03@gmail.com>
2017-11-18 19:25:04 +0700

    app-7

--- function.js
+++ function.js
@@ -49,3 +49,19 @@
          message: 'hello vue!'
      }
  })
+ 
+ Vue.component('todo-item', {
+     props:['todo'],
+     template: '<li>{{ todo.text }}</li>'
+ })
+
+var app7 =new Vue ({
+    el:'#app-7',
+    data:{
+        groceryList: [
+            { id:0, text:'vegetables'},
+            { id:1, text:'cheese'},
+            { id:2, text:'whatever'}
+        ]
+    }
+})
