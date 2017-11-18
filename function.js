678c997a591618bc00ccab819e8234888f5611ca
annisha <channisha03@gmail.com>
2017-11-18 19:17:10 +0700

    sampai app-6

--- function.js
+++ function.js
@@ -11,3 +11,41 @@
          message:'you loaded this page on '+ new Date().toLocaleString()
      }
  })
+ 
+ var app3= new Vue({
+     el:'#app-3',
+     data:{
+         seen:true
+     }
+ })
+ 
+ var app4 = new Vue ({
+     el: '#app-4',
+     data:{
+         todos:[
+             { text: 'learn javascript'},
+             { text: 'learn vue'},
+             { text: 'build something awesome'}
+             
+         ]
+     }
+ })
+ 
+ var app5 = new Vue ({
+     el: '#app-5',
+     data: {
+         message: 'hello vue.js!'
+     },
+     methods: {
+         reverseMessage: function(){
+             this.message =this.message.split('').reverse().join('')
+         }
+     }
+ })
+ 
+ var app6 =new Vue({
+     el:'#app-6',
+     data: {
+         message: 'hello vue!'
+     }
+ })
